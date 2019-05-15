436bb517d10f84bef84360c71da24c2196690565
deathflame1515 <ernest.mangune@gmail.com>
2019-05-15 13:25:10 +0800

    bug fixed

--- app/Http/Controllers/TechbagIteneraryController.php
+++ app/Http/Controllers/TechbagIteneraryController.php
@@ -25,8 +25,8 @@ class TechbagIteneraryController extends Controller
         $location = DB::table('items')->select('location')->groupBy('location')->where('location','like','Tech Bag%')
         ->whereNotIn("location",TechbagItenerary::select("location")->whereNull("date_in"))->get();
         $local = DB::table('techbag_itenerary')->select('location')->groupBy('location')->get();
-        $employ = DB::table('users')->select('name')->where('designation', 'Network and Service Partner')
-        ->orWhere('designation', 'Software Development')->get();
+        $employ = DB::table('users')->select('name')->where('designation', 'like', 'Network Service Partner')
+        ->orWhere('designation', 'like', 'Software Development')->get();
         return view('techbag.index')->with(['techbags'=>$techbags, 'location'=>$location, 'local'=>$local, 'employ'=>$employ]);
 
     }
